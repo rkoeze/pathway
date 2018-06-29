a5b4c4a3ca6355732b21fb98a874a6e1a7befd7f
rkoeze <rkoeze@gmail.com>
2018-06-29 12:36:51 -0700

    Begin adding save, edit, and delete functionality.

--- pathway.sh
+++ pathway.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 function set_aliases() {
 
   : > "${SCRIPT_DIR}/SORTED_COMMANDS.txt"
-  cat "${SCRIPT_DIR}/COMMAND_LOG.txt" | sort | uniq -c >> "${SCRIPT_DIR}/COMMAND_LOG.txt"
+  cat "${SCRIPT_DIR}/COMMAND_LOG.txt" | sort | uniq -c >> "${SCRIPT_DIR}/SORTED_COMMANDS.txt"
 
   IFS=$'\n'
   for l in $(cat "${SCRIPT_DIR}/SORTED_COMMANDS.txt")
@@ -23,22 +23,30 @@ function set_aliases() {
 
 i=0
 
+FREEZE=false
+
 function capture_input() {
-  i=$(($i+1))
+  if [ "$FREEZE" = "false" ]; then
+    i=$(($i+1))
 
-  echo "current count $i"
+    echo "current count $i"
 
-  if [ $(($i%10)) == 0 ]; then
-    set_aliases
+    if [ $(($i%10)) == 0 ]; then
+      set_aliases
+    fi
+
+    echo $PWD >> "${SCRIPT_DIR}/COMMAND_LOG.txt"
   fi
-
-  echo $PWD >> ${SCRIPT_DIR}/TESTHISTFILE.txt
 }
 
 
 function pw() {
+  declare opt
+  declare OPTARG
+  declare OPTIND
+
   echo "trying to work"
-  while getopts "cd" opt; do
+  while getopts "cns:d:fu" opt; do
     echo "working"
     case $opt in
       "c")
@@ -47,8 +55,23 @@ function pw() {
         : > "${SCRIPT_DIR}/SORTED_COMMANDS.txt"
         : > "${SCRIPT_DIR}/COMMAND_LOG.txt";;
       "s")
+        new_alias=${OPTARG}
+        alias_filepath=$(cat SORTED_COMMANDS.txt | sed -rn '/notes+$/p' | sed -r "s/.*\s.*[0-9]\s//g")
+        cat $("${new_alias} = cd ${alias_filepath}" >> "${SCRIPT_DIR}/SAVED.txt")
         ;;
       "d")
+        old_alias=${OPTARG}
+        unalias $old_alias
+        ;;
+      "f")
+        if [ "$FREEZE" = "false" ]; then
+          $FREEZE=true
+        fi
+        ;;
+      "u")
+        if [ "$FREEZE" = "true" ]; then
+          $FREEZE=false
+        fi
         ;;
     esac
   done
@@ -56,5 +79,14 @@ function pw() {
 
 export -f pw
 
+function load_aliases() {
+  IFS=$'\n'
+  for l in $(cat "${SCRIPT_DIR}/SAVED.txt")
+  do
+    eval $l
+  done
+}
 
+load_aliases
+set_aliases
 PROMPT_COMMAND="$PROMPT_COMMAND capture_input"
